8e9ad8f3116f71086c2cf98d6b33d098b591528c
johny0120 <70541372+johny0120@users.noreply.github.com>
2020-12-29 11:38:33 +0530

    Initial commit

---
+++ DataType.java
@@ -0,0 +1,43 @@
+
+public class DataType {
+public static void main(String[] args) {
+	
+	//byte
+	 byte age= 26;
+	 System.out.println("Age Value:"+age);
+	 
+	 //short
+	 short empid=1645;
+	 System.out.println("Emp Id:"+empid);
+	
+	 // int
+	 int reference=164591;
+	 System.out.println("reference num:"+reference);
+	 
+	 //long
+	 long avg=9551387738l;
+	 System.out.println("Average is;"+avg);
+	 
+	 //float
+	 float average=98.01f;
+	 System.out.println("avg:"+average);
+	 
+	 //double
+	 double salary=700000;
+	 System.out.println("My Salary Is;"+salary);
+	 
+	 //char
+	 char block='A';
+	 System.out.println("What is Yiour Block:"+block);
+	 
+	 //boolean
+	 boolean empstatus=true;
+	 System.out.println("Emp Status:"+empstatus);
+	 
+	 //String
+	  String name="johnyboy2471997@gmail.com";
+	  System.out.println("Add Your Gmail"+name);
+	  
+	 
+}
+}
